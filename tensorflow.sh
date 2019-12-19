69c987551c10787c3f459139e979f714882a087b
hylian-flute <hylian_flute@yahoo.co.jp>
2019-12-19 16:06:39 +0900

    tensorflowをGPUに対応

--- tensorflow.sh
+++ tensorflow.sh
@@ -1,2 +1,2 @@
-docker run -it --rm -u $SUDO_UID:$SUDO_GID -v `pwd`:/home/tensorflow/ -w /home/tensorflow/ charines/tensorflow python3 $1
+docker run -it --rm --runtime=nvidia -u $SUDO_UID:$SUDO_GID -v `pwd`:/home/tensorflow/ -w /home/tensorflow/ charines/tensorflow:gpu python3 $1
 
